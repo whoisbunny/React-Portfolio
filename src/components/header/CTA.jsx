f52f96acbd9d9401c9721ec6a87c29bf8ef0672f
Bunny <kamleshrathod2092000@gmail.com>
2023-07-21 14:09:09 +0530

    Add resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+import Resume from "https://drive.google.com/file/d/1O_eCCp25ZB4SM3Wi827fln78o4vsK7ly/view?usp=drive_link";
 
 const CTA = () => {
   return (
-    <div className='cta'>
-      {/* <a href={CV} download className="btn">Download CV</a> */}
-      <a href={'#'}  className="btn">Download CV</a>
-      <a href="#contact" className="btn btn-primary"> Contact Me</a>
+    <div className="cta">
+      <a href={Resume} download className="btn">
+        Resume
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        {" "}
+        Contact Me
+      </a>
     </div>
-  )
+  );
 }
 
 export default CTA
