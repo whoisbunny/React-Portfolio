bc16f61ebe79194acefb58e2bdb174de32620afe
Kamlesh Rathod <132698748+whoisbunny@users.noreply.github.com>
2023-09-02 02:43:23 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
     <div className="cta">
       <a
         href={
-          "https://drive.google.com/file/d/1O_eCCp25ZB4SM3Wi827fln78o4vsK7ly/view?usp=drive_link"
+          "https://drive.google.com/file/d/1XDyga7UP2XXei4ICcI_-E6SO_5ZMbb65/view?usp=sharing"
         }
         download
         className="btn"
