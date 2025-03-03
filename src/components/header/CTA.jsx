396db2fa397d011c9570b4b2e61aa047f1ac6ee4
Bunny <kamleshrathod2092000@gmail.com>
2025-03-03 17:47:22 +0530

    Update : Profile

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
     <div className="cta">
       <a
         href={
-          "https://drive.google.com/file/d/1-a1IbEl-OnUBF2bhc66XdtGSuRgXz9TC/view?usp=sharing"
+          "https://drive.google.com/file/d/1FWJDmcxq5AceQ86mSyJ5ZhaDvdezIoIy/view?usp=sharing"
         }
         download
         className="btn"
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import ME from '../../assets/bunny1.png'
-import HeaderSocials from './HeaderSocials'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/Profile.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
     <header>
-
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Kamlesh Rathod</h1>
-        <h5 className='text-light'>
-          Web Developer
-        </h5>
-      <CTA  />
+        <h5 className="text-light">Web Developer</h5>
+        <CTA />
 
-      <HeaderSocials/>
-      <div className="me">
-        <img src={ME } alt="" className='header__img' />
-
-      </div>
-      <a href="#contact" className='scroll__down'> Scroll Down</a>
+        <HeaderSocials />
+        <div className="flex_center">
+          <div className="me">
+            <img src={ME} alt="" className="header__img" />
+          </div>
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
