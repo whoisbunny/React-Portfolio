8264a4e1e5f7633e17145f97e12c35ba26f26423
Bunny <kamleshrathod2092000@gmail.com>
2025-03-19 21:53:32 +0530

    Update :: Code

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import "./portfolio.css";
 import { projects } from "../../data";
-// import Swiper core and required modules
-import { Pagination } from "swiper";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-
-import "swiper/css/pagination";
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My recent work</h5>
       <h2>Portfolio</h2>
-      {/* <div className="container portfolio__container">
+      <div className="container portfolio__container">
         {projects.map(({ id, title, description, image, liveDemo, github }) => (
           <article key={id} className="portfolio__item">
             <div className="portfolio__item-image">
@@ -38,39 +30,8 @@ const Portfolio = () => {
             </div>
           </article>
         ))}
-      </div> */}
-      <Swiper
-        className="container portfolio__container"
-        // install Swiper modules
-        modules={[Pagination]}
-        spaceBetween={40}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-      >
-        {projects.map(({ id, title, description, image, liveDemo, github }) => (
-          <SwiperSlide key={id} className="portfolio__item">
-            <div className="portfolio__item-image">
-              <img src={image} alt={title} />
-            </div>
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <div className="portfolio__item-cta">
-              <a href={github} className="btn" target="_blank" rel="noreferrer">
-                Github
-              </a>
-              <a
-                href={liveDemo}
-                className="btn btn-primary"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Demo
-              </a>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      </div>
+     
     </section>
   );
 };
